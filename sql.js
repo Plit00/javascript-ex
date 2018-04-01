4cff5aca77fce2d6963a101cc080f5f2c24e819a
Plit00 <eovyxla14@gmail.com>
2018-04-01 21:08:59 +0900

    Create sql.js

---
+++ sql.js
@@ -0,0 +1,23 @@
+function worker(name , age)
+{
+	var strName = name;
+	car intRank = rank;
+    student.prototype.getName() {return thus.strName;}
+    student.prototype.setName(name) {this.strName = name; }
+    student.prototype.getRank() {return this.setRank;}
+    student.prototype.setRank(rank) {this.intRank = rank; }
+}
+
+var mycomputer = new class ({
+  initialize function(name){
+  	this.name = name;
+  });
+
+var mycomputer = new mac("macs");
+alert(mycomputer.name);
+
+var.cow = new class({
+	initialize: function(){
+		alert('cow_initialize');
+	}
+});
